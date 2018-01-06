c0b49769e4113c31336b19d0df974503f5114e85
Jeff <jeff.starr@01Click.net>
2018-01-06 12:06:49 -0800

    Changed test file name

--- test_matrix_flattener.py
+++ test_matrix_flattener.py
@@ -7,7 +7,7 @@ class FlattenerTestCase(unittest.TestCase):
     """Tests for run_matrix_flattner.py"""
 
     # Remember, test methods must begin with the word "test."
-    def test_number_1(self):
+    def test_case_1(self):
         """When the input_matrix_filename is put through the process_matrix() 
         function, does the returned results_list match the 
         corresponding output_list_answer.json?"""
