ff12a58a462bb449e41d6489afa132853d4bb34d
Jeff <jeff.starr@01Click.net>
2018-01-06 11:41:16 -0800

    Added a test

--- run_matrix_flattener.py
+++ run_matrix_flattener.py
@@ -10,13 +10,13 @@ from matrix_flattener_function import process_matrix
 
 input_matrix_filename = os.path.expanduser(
     "~/Documents/Coding/Projects/" +
-    "PRODUCTION_DATA/Matrix_Flattener_data/input_matrix_test.csv"
+    "PRODUCTION_DATA/Matrix_Flattener_data/input_matrix_test1.csv"
     )
 
 
 output_list_filename = os.path.expanduser(
     "~/Documents/Coding/Projects/PRODUCTION_DATA/" +
-    "Matrix_Flattener_data/output_list_test.csv"
+    "Matrix_Flattener_data/output_list_test1.csv"
     )
 
 output_list = process_matrix(input_matrix_filename)
@@ -29,3 +29,16 @@ with open(output_list_filename, 'w', encoding='utf-8') as f_write:
     writer = csv.writer(f_write)
     for row in output_list:
         writer.writerow([row])
+
+
+# The following writes the output_list as json to a file so that it can be
+# checked by a human then used for testing.
+
+"""
+output_json_filename = os.path.expanduser(
+    "~/Documents/Coding/Projects/" +
+    "Matrix_Flattener/test_files/output_list_answer1.json"
+    )
+with open(output_json_filename, 'w') as outfile:
+    json.dump(output_list, outfile)
+"""
--- test_run_matrix_flattener.py
+++ test_run_matrix_flattener.py
@@ -3,31 +3,23 @@ import json
 import csv
 from matrix_flattener_function import process_matrix
 
-class MatrixTestCase(unittest.TestCase):
-    """Tests for links_matrix_function.py"""
+class FlattenerTestCase(unittest.TestCase):
+    """Tests for run_matrix_flattner.py"""
 
-    def test_first_input(self):
-        """Do the first test input and history matrices return 
-        correct results."""
+    # Remember, test methods must begin with the word "test."
+    def test_the_first(self):
+        """When the input_matrix_filename is put through the process_matrix() 
+        function, does the returned results_list match the 
+        corresponding output_list_answer.json?"""
         
-        links_input_filename = 'test_files/link_matrix_input1.csv' 
-        links_output_filename = 'test_files/output_records1.csv'
-        history_input_filename = 'test_files/links_history0.csv'
-        history_output_filename = 'test_files/links_history1.csv'
+        input_matrix_filename1 = 'test_files/input_matrix_test1.csv' 
+      
+        results_list1 = process_matrix(input_matrix_filename1)
 
-        results1 = process_links_matrix(
-            links_input_filename,
-            history_input_filename)
+        right_answer_file1 = 'test_files/output_list_answer1.json'
+        with open(right_answer_file1) as f_obj:
+            right_answer1 = json.load(f_obj)
 
-        filename1 = 'test_files/output_matrix1.json'
-        with open(filename1) as f_obj:
-            om1_right_answer = json.load(f_obj)
-
-        filename2 = 'test_files/history_matrix1.json'
-        with open(filename2) as f_obj:
-            hm1_right_answer = json.load(f_obj)
-        
-        self.assertEqual(results1, (om1_right_answer,hm1_right_answer))
-
+        self.assertEqual(results_list1,right_answer1)
 
 unittest.main()
